70a2a2376b7ca97df3eddff82372d32af492bc89
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2021-01-30 06:46:36 +0700

    add rest_framework, safedelete, healthcheck to settings

--- LineBotBackend/settings.py
+++ LineBotBackend/settings.py
@@ -42,13 +42,16 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'corsheaders',
-    'bot.apps.BotConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'health_check',
+    'safedelete',
+    'bot',
     'home'
 ]
 
